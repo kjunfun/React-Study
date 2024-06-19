f90dc08a7d58a8367ca57e6949b160f31349d156
JHLee <term273729@gmail.com>
2024-06-19 16:04:42 +0900

    React State & Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,27 +1,15 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 
-//App Component (함수 컴포넌트 형태)
+//함수 컴포넌트에서 state 생성
+import { useState } from "react";
+
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
   return (
     <>
-      {/* 각 개별 요소가 props */}
-      {/* JS ES6 문법인 스프레드 연산자(...) 적용하여 Button component의 props로 전달 */}
-      <Button {...buttonProps}></Button>
-      <Button text={"카페"}></Button>
-      <Button text={"블로그"}>
-        <Header></Header>
-      </Button>
+      <Bulb></Bulb>
+      <Counter></Counter>
     </>
   );
 }
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -5,16 +5,12 @@ const Button = ({ text, color = "black", children }) => {
     console.log(e);
     console.log(text);
   };
-  // const handleMouseEnter = () => {
-  //   console.log("Mouse entered:", text);
-  // };
 
   return (
     <button
       //이벤트 핸들러: 이벤트를 실질적으로 처리하는 함수.
       ///이벤트 핸들러를 설정할 때, 함수를 호출하는 식으로 ()를 붙이는 것이 아니라, 콜백함수를 전달하듯이 이름만 이벤트 핸들러로 설정.
       onClick={handleClick}
-      // onMouseEnter={handleMouseEnter}
       style={{ color: color }}
     >
       {text} - {color.toUpperCase()}
@@ -22,9 +18,5 @@ const Button = ({ text, color = "black", children }) => {
     </button>
   );
 };
-
-// Button.defaultProps = {
-//   color: "black",
-// };
 
 export default Button;
