4df3a629b25c479d96145c2c3a0b4233be7fb6d2
JHLee <term273729@gmail.com>
2024-06-19 17:49:33 +0900

    registration form using React State

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
-
-//함수 컴포넌트에서 state 생성
-import { useState } from "react";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Bulb></Bulb>
-      <Counter></Counter>
+      <Register></Register>
     </>
   );
 }
